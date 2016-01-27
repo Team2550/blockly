d1d643577609cf1125507b14494465f172314603
NWalker1208 <nathanwalker1208@gmail.com>
2016-01-26 18:24:43 -0800

    Added RoboRio code and removed other potential conflicts
    ToDo: Customize code for c++

--- generators/roborio.js
+++ generators/roborio.js
@@ -24,16 +24,16 @@
  */
 'use strict';
 
-goog.provide('Blockly.JavaScript');
+goog.provide('Blockly.RoboRio');
 
 goog.require('Blockly.Generator');
 
 
 /**
- * JavaScript code generator.
+ * RoboRio code generator.
  * @type {!Blockly.Generator}
  */
-Blockly.JavaScript = new Blockly.Generator('JavaScript');
+Blockly.RoboRio = new Blockly.Generator('RoboRio');
 
 /**
  * List of illegal variable names.
@@ -42,7 +42,7 @@ Blockly.JavaScript = new Blockly.Generator('JavaScript');
  * accidentally clobbering a built-in object or function.
  * @private
  */
-Blockly.JavaScript.addReservedWords(
+Blockly.RoboRio.addReservedWords(
     'Blockly,' +  // In case JS is evaled in the current window.
     // https://developer.mozilla.org/en/JavaScript/Reference/Reserved_Words
     'break,case,catch,continue,debugger,default,delete,do,else,finally,for,function,if,in,instanceof,new,return,switch,this,throw,try,typeof,var,void,while,with,' +
@@ -68,67 +68,66 @@ Blockly.JavaScript.addReservedWords(
 
 /**
  * Order of operation ENUMs.
- * https://developer.mozilla.org/en/JavaScript/Reference/Operators/Operator_Precedence
+ * http://en.cppreference.com/w/cpp/language/operator_precedence
  */
-Blockly.JavaScript.ORDER_ATOMIC = 0;         // 0 "" ...
-Blockly.JavaScript.ORDER_MEMBER = 1;         // . []
-Blockly.JavaScript.ORDER_NEW = 1;            // new
-Blockly.JavaScript.ORDER_FUNCTION_CALL = 2;  // ()
-Blockly.JavaScript.ORDER_INCREMENT = 3;      // ++
-Blockly.JavaScript.ORDER_DECREMENT = 3;      // --
-Blockly.JavaScript.ORDER_LOGICAL_NOT = 4;    // !
-Blockly.JavaScript.ORDER_BITWISE_NOT = 4;    // ~
-Blockly.JavaScript.ORDER_UNARY_PLUS = 4;     // +
-Blockly.JavaScript.ORDER_UNARY_NEGATION = 4; // -
-Blockly.JavaScript.ORDER_TYPEOF = 4;         // typeof
-Blockly.JavaScript.ORDER_VOID = 4;           // void
-Blockly.JavaScript.ORDER_DELETE = 4;         // delete
-Blockly.JavaScript.ORDER_MULTIPLICATION = 5; // *
-Blockly.JavaScript.ORDER_DIVISION = 5;       // /
-Blockly.JavaScript.ORDER_MODULUS = 5;        // %
-Blockly.JavaScript.ORDER_ADDITION = 6;       // +
-Blockly.JavaScript.ORDER_SUBTRACTION = 6;    // -
-Blockly.JavaScript.ORDER_BITWISE_SHIFT = 7;  // << >> >>>
-Blockly.JavaScript.ORDER_RELATIONAL = 8;     // < <= > >=
-Blockly.JavaScript.ORDER_IN = 8;             // in
-Blockly.JavaScript.ORDER_INSTANCEOF = 8;     // instanceof
-Blockly.JavaScript.ORDER_EQUALITY = 9;       // == != === !==
-Blockly.JavaScript.ORDER_BITWISE_AND = 10;   // &
-Blockly.JavaScript.ORDER_BITWISE_XOR = 11;   // ^
-Blockly.JavaScript.ORDER_BITWISE_OR = 12;    // |
-Blockly.JavaScript.ORDER_LOGICAL_AND = 13;   // &&
-Blockly.JavaScript.ORDER_LOGICAL_OR = 14;    // ||
-Blockly.JavaScript.ORDER_CONDITIONAL = 15;   // ?:
-Blockly.JavaScript.ORDER_ASSIGNMENT = 16;    // = += -= *= /= %= <<= >>= ...
-Blockly.JavaScript.ORDER_COMMA = 17;         // ,
-Blockly.JavaScript.ORDER_NONE = 99;          // (...)
+Blockly.RoboRio.ORDER_ATOMIC = 0;           // 0 "" ...
+Blockly.RoboRio.ORDER_SCOPE_RESOLUTION = 1; // 0 "" ...
+Blockly.RoboRio.ORDER_MEMBER = 2;           // . []
+Blockly.RoboRio.ORDER_FUNCTION_CALL = 2;    // ()
+Blockly.RoboRio.ORDER_NEW = 3;              // new
+Blockly.RoboRio.ORDER_INCREMENT = 3;        // ++
+Blockly.RoboRio.ORDER_DECREMENT = 3;        // --
+Blockly.RoboRio.ORDER_LOGICAL_NOT = 3;      // !
+Blockly.RoboRio.ORDER_BITWISE_NOT = 3;      // ~
+Blockly.RoboRio.ORDER_UNARY_PLUS = 3;       // +
+Blockly.RoboRio.ORDER_UNARY_NEGATION = 3;   // -
+Blockly.RoboRio.ORDER_VOID = 3;             // void
+Blockly.RoboRio.ORDER_DELETE = 3;           // delete
+Blockly.RoboRio.ORDER_POINTER = 4           // .* ->*
+Blockly.RoboRio.ORDER_MULTIPLICATION = 5;   // *
+Blockly.RoboRio.ORDER_DIVISION = 5;         // /
+Blockly.RoboRio.ORDER_MODULUS = 5;          // %
+Blockly.RoboRio.ORDER_ADDITION = 6;         // +
+Blockly.RoboRio.ORDER_SUBTRACTION = 6;      // -
+Blockly.RoboRio.ORDER_BITWISE_SHIFT = 7;    // << >> >>>
+Blockly.RoboRio.ORDER_RELATIONAL = 8;       // < <= > >=
+Blockly.RoboRio.ORDER_EQUALITY = 9;         // == != === !==
+Blockly.RoboRio.ORDER_BITWISE_AND = 10;     // &
+Blockly.RoboRio.ORDER_BITWISE_XOR = 11;     // ^
+Blockly.RoboRio.ORDER_BITWISE_OR = 12;      // |
+Blockly.RoboRio.ORDER_LOGICAL_AND = 13;     // &&
+Blockly.RoboRio.ORDER_LOGICAL_OR = 14;      // ||
+Blockly.RoboRio.ORDER_CONDITIONAL = 15;     // ?:
+Blockly.RoboRio.ORDER_ASSIGNMENT = 15;      // = += -= *= /= %= <<= >>= ...
+Blockly.RoboRio.ORDER_COMMA = 16;           // ,
+Blockly.RoboRio.ORDER_NONE = 99;            // (...)
 
 /**
  * Initialise the database of variable names.
  * @param {!Blockly.Workspace} workspace Workspace to generate code from.
  */
-Blockly.JavaScript.init = function(workspace) {
+Blockly.RoboRio.init = function(workspace) {
   // Create a dictionary of definitions to be printed before the code.
-  Blockly.JavaScript.definitions_ = Object.create(null);
+  Blockly.RoboRio.definitions_ = Object.create(null);
   // Create a dictionary mapping desired function names in definitions_
   // to actual function names (to avoid collisions with user functions).
-  Blockly.JavaScript.functionNames_ = Object.create(null);
+  Blockly.RoboRio.functionNames_ = Object.create(null);
 
-  if (!Blockly.JavaScript.variableDB_) {
-    Blockly.JavaScript.variableDB_ =
-        new Blockly.Names(Blockly.JavaScript.RESERVED_WORDS_);
+  if (!Blockly.RoboRio.variableDB_) {
+    Blockly.RoboRio.variableDB_ =
+        new Blockly.Names(Blockly.RoboRio.RESERVED_WORDS_);
   } else {
-    Blockly.JavaScript.variableDB_.reset();
+    Blockly.RoboRio.variableDB_.reset();
   }
 
   var defvars = [];
   var variables = Blockly.Variables.allVariables(workspace);
   for (var i = 0; i < variables.length; i++) {
-    defvars[i] = 'var ' +
-        Blockly.JavaScript.variableDB_.getName(variables[i],
+    defvars[i] = 'string ' +
+        Blockly.RoboRio.variableDB_.getName(variables[i],
         Blockly.Variables.NAME_TYPE) + ';';
   }
-  Blockly.JavaScript.definitions_['variables'] = defvars.join('\n');
+  Blockly.RoboRio.definitions_['variables'] = defvars.join('\n');
 };
 
 /**
@@ -136,16 +135,16 @@ Blockly.JavaScript.init = function(workspace) {
  * @param {string} code Generated code.
  * @return {string} Completed code.
  */
-Blockly.JavaScript.finish = function(code) {
+Blockly.RoboRio.finish = function(code) {
   // Convert the definitions dictionary into a list.
   var definitions = [];
-  for (var name in Blockly.JavaScript.definitions_) {
-    definitions.push(Blockly.JavaScript.definitions_[name]);
+  for (var name in Blockly.RoboRio.definitions_) {
+    definitions.push(Blockly.RoboRio.definitions_[name]);
   }
   // Clean up temporary data.
-  delete Blockly.JavaScript.definitions_;
-  delete Blockly.JavaScript.functionNames_;
-  Blockly.JavaScript.variableDB_.reset();
+  delete Blockly.RoboRio.definitions_;
+  delete Blockly.RoboRio.functionNames_;
+  Blockly.RoboRio.variableDB_.reset();
   return definitions.join('\n\n') + '\n\n\n' + code;
 };
 
@@ -155,7 +154,7 @@ Blockly.JavaScript.finish = function(code) {
  * @param {string} line Line of generated code.
  * @return {string} Legal line of code.
  */
-Blockly.JavaScript.scrubNakedValue = function(line) {
+Blockly.RoboRio.scrubNakedValue = function(line) {
   return line + ';\n';
 };
 
@@ -166,7 +165,7 @@ Blockly.JavaScript.scrubNakedValue = function(line) {
  * @return {string} JavaScript string.
  * @private
  */
-Blockly.JavaScript.quote_ = function(string) {
+Blockly.RoboRio.quote_ = function(string) {
   // TODO: This is a quick hack.  Replace with goog.string.quote
   string = string.replace(/\\/g, '\\\\')
                  .replace(/\n/g, '\\\n')
@@ -183,14 +182,14 @@ Blockly.JavaScript.quote_ = function(string) {
  * @return {string} JavaScript code with comments and subsequent blocks added.
  * @private
  */
-Blockly.JavaScript.scrub_ = function(block, code) {
+Blockly.RoboRio.scrub_ = function(block, code) {
   var commentCode = '';
   // Only collect comments for blocks that aren't inline.
   if (!block.outputConnection || !block.outputConnection.targetConnection) {
     // Collect comment for this block.
     var comment = block.getCommentText();
     if (comment) {
-      commentCode += Blockly.JavaScript.prefixLines(comment, '// ') + '\n';
+      commentCode += Blockly.RoboRio.prefixLines(comment, '// ') + '\n';
     }
     // Collect comments for all value arguments.
     // Don't collect comments for nested statements.
@@ -198,15 +197,15 @@ Blockly.JavaScript.scrub_ = function(block, code) {
       if (block.inputList[x].type == Blockly.INPUT_VALUE) {
         var childBlock = block.inputList[x].connection.targetBlock();
         if (childBlock) {
-          var comment = Blockly.JavaScript.allNestedComments(childBlock);
+          var comment = Blockly.RoboRio.allNestedComments(childBlock);
           if (comment) {
-            commentCode += Blockly.JavaScript.prefixLines(comment, '// ');
+            commentCode += Blockly.RoboRio.prefixLines(comment, '// ');
           }
         }
       }
     }
   }
   var nextBlock = block.nextConnection && block.nextConnection.targetBlock();
-  var nextCode = Blockly.JavaScript.blockToCode(nextBlock);
+  var nextCode = Blockly.RoboRio.blockToCode(nextBlock);
   return commentCode + code + nextCode;
 };
--- generators/roborio/logic.js
+++ generators/roborio/logic.js
@@ -19,37 +19,37 @@
  */
 
 /**
- * @fileoverview Generating JavaScript for logic blocks.
+ * @fileoverview Generating C++ for logic blocks.
  * @author q.neutron@gmail.com (Quynh Neutron)
  */
 'use strict';
 
-goog.provide('Blockly.JavaScript.logic');
+goog.provide('Blockly.RoboRio.logic');
 
-goog.require('Blockly.JavaScript');
+goog.require('Blockly.RoboRio');
 
 
-Blockly.JavaScript['controls_if'] = function(block) {
+Blockly.RoboRio['controls_if'] = function(block) {
   // If/elseif/else condition.
   var n = 0;
-  var argument = Blockly.JavaScript.valueToCode(block, 'IF' + n,
-      Blockly.JavaScript.ORDER_NONE) || 'false';
-  var branch = Blockly.JavaScript.statementToCode(block, 'DO' + n);
+  var argument = Blockly.RoboRio.valueToCode(block, 'IF' + n,
+      Blockly.RoboRio.ORDER_NONE) || 'false';
+  var branch = Blockly.RoboRio.statementToCode(block, 'DO' + n);
   var code = 'if (' + argument + ') {\n' + branch + '}';
   for (n = 1; n <= block.elseifCount_; n++) {
-    argument = Blockly.JavaScript.valueToCode(block, 'IF' + n,
-        Blockly.JavaScript.ORDER_NONE) || 'false';
-    branch = Blockly.JavaScript.statementToCode(block, 'DO' + n);
+    argument = Blockly.RoboRio.valueToCode(block, 'IF' + n,
+        Blockly.RoboRio.ORDER_NONE) || 'false';
+    branch = Blockly.RoboRio.statementToCode(block, 'DO' + n);
     code += ' else if (' + argument + ') {\n' + branch + '}';
   }
   if (block.elseCount_) {
-    branch = Blockly.JavaScript.statementToCode(block, 'ELSE');
+    branch = Blockly.RoboRio.statementToCode(block, 'ELSE');
     code += ' else {\n' + branch + '}';
   }
   return code + '\n';
 };
 
-Blockly.JavaScript['logic_compare'] = function(block) {
+Blockly.RoboRio['logic_compare'] = function(block) {
   // Comparison operator.
   var OPERATORS = {
     'EQ': '==',
@@ -61,20 +61,20 @@ Blockly.JavaScript['logic_compare'] = function(block) {
   };
   var operator = OPERATORS[block.getFieldValue('OP')];
   var order = (operator == '==' || operator == '!=') ?
-      Blockly.JavaScript.ORDER_EQUALITY : Blockly.JavaScript.ORDER_RELATIONAL;
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'A', order) || '0';
-  var argument1 = Blockly.JavaScript.valueToCode(block, 'B', order) || '0';
+      Blockly.RoboRio.ORDER_EQUALITY : Blockly.RoboRio.ORDER_RELATIONAL;
+  var argument0 = Blockly.RoboRio.valueToCode(block, 'A', order) || '0';
+  var argument1 = Blockly.RoboRio.valueToCode(block, 'B', order) || '0';
   var code = argument0 + ' ' + operator + ' ' + argument1;
   return [code, order];
 };
 
-Blockly.JavaScript['logic_operation'] = function(block) {
+Blockly.RoboRio['logic_operation'] = function(block) {
   // Operations 'and', 'or'.
   var operator = (block.getFieldValue('OP') == 'AND') ? '&&' : '||';
-  var order = (operator == '&&') ? Blockly.JavaScript.ORDER_LOGICAL_AND :
-      Blockly.JavaScript.ORDER_LOGICAL_OR;
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'A', order);
-  var argument1 = Blockly.JavaScript.valueToCode(block, 'B', order);
+  var order = (operator == '&&') ? Blockly.RoboRio.ORDER_LOGICAL_AND :
+      Blockly.RoboRio.ORDER_LOGICAL_OR;
+  var argument0 = Blockly.RoboRio.valueToCode(block, 'A', order);
+  var argument1 = Blockly.RoboRio.valueToCode(block, 'B', order);
   if (!argument0 && !argument1) {
     // If there are no arguments, then the return value is false.
     argument0 = 'false';
@@ -93,34 +93,34 @@ Blockly.JavaScript['logic_operation'] = function(block) {
   return [code, order];
 };
 
-Blockly.JavaScript['logic_negate'] = function(block) {
+Blockly.RoboRio['logic_negate'] = function(block) {
   // Negation.
-  var order = Blockly.JavaScript.ORDER_LOGICAL_NOT;
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'BOOL', order) ||
+  var order = Blockly.RoboRio.ORDER_LOGICAL_NOT;
+  var argument0 = Blockly.RoboRio.valueToCode(block, 'BOOL', order) ||
       'true';
   var code = '!' + argument0;
   return [code, order];
 };
 
-Blockly.JavaScript['logic_boolean'] = function(block) {
+Blockly.RoboRio['logic_boolean'] = function(block) {
   // Boolean values true and false.
   var code = (block.getFieldValue('BOOL') == 'TRUE') ? 'true' : 'false';
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+  return [code, Blockly.RoboRio.ORDER_ATOMIC];
 };
 
-Blockly.JavaScript['logic_null'] = function(block) {
+Blockly.RoboRio['logic_null'] = function(block) {
   // Null data type.
-  return ['null', Blockly.JavaScript.ORDER_ATOMIC];
+  return ['null', Blockly.RoboRio.ORDER_ATOMIC];
 };
 
-Blockly.JavaScript['logic_ternary'] = function(block) {
+Blockly.RoboRio['logic_ternary'] = function(block) {
   // Ternary operator.
-  var value_if = Blockly.JavaScript.valueToCode(block, 'IF',
-      Blockly.JavaScript.ORDER_CONDITIONAL) || 'false';
-  var value_then = Blockly.JavaScript.valueToCode(block, 'THEN',
-      Blockly.JavaScript.ORDER_CONDITIONAL) || 'null';
-  var value_else = Blockly.JavaScript.valueToCode(block, 'ELSE',
-      Blockly.JavaScript.ORDER_CONDITIONAL) || 'null';
+  var value_if = Blockly.RoboRio.valueToCode(block, 'IF',
+      Blockly.RoboRio.ORDER_CONDITIONAL) || 'false';
+  var value_then = Blockly.RoboRio.valueToCode(block, 'THEN',
+      Blockly.RoboRio.ORDER_CONDITIONAL) || 'null';
+  var value_else = Blockly.RoboRio.valueToCode(block, 'ELSE',
+      Blockly.RoboRio.ORDER_CONDITIONAL) || 'null';
   var code = value_if + ' ? ' + value_then + ' : ' + value_else;
-  return [code, Blockly.JavaScript.ORDER_CONDITIONAL];
+  return [code, Blockly.RoboRio.ORDER_CONDITIONAL];
 };
